23aa70156f5e2036feb27271548e9cfacf018244
Carl Jackson <carl@avtok.com>
2016-02-21 02:11:40 -0800

    internal: pre-bake interface{} objects
    This nets us a very impressive 12-20% speedup in a microbenchmark on my
    computer (Early 2013 Retina MBP, 2.8GHz, running Go 1.6).
    
    name            old time/op    new time/op    delta
    EndToEnd5-8        254ns ± 6%     203ns ±19%  -20.06%  (p=0.000 n=17+20)
    EndToEnd50-8       268ns ±11%     224ns ±11%  -16.67%  (p=0.000 n=19+18)
    EndToEnd500-8      307ns ±15%     267ns ± 9%  -12.84%  (p=0.000 n=20+17)
    EndToEnd5000-8     393ns ±12%     337ns ±14%  -14.37%  (p=0.000 n=20+20)
    
    name            old alloc/op   new alloc/op   delta
    EndToEnd5-8         203B ± 0%      169B ± 0%  -16.75%  (p=0.000 n=20+20)
    EndToEnd50-8        203B ± 0%      169B ± 0%  -16.75%  (p=0.000 n=20+20)
    EndToEnd500-8       203B ± 0%      169B ± 0%  -16.75%  (p=0.000 n=20+20)
    EndToEnd5000-8      203B ± 0%      169B ± 0%  -16.75%  (p=0.000 n=20+20)
    
    name            old allocs/op  new allocs/op  delta
    EndToEnd5-8         8.00 ± 0%      4.00 ± 0%  -50.00%  (p=0.000 n=20+20)
    EndToEnd50-8        8.00 ± 0%      4.00 ± 0%  -50.00%  (p=0.000 n=20+20)
    EndToEnd500-8       8.00 ± 0%      4.00 ± 0%  -50.00%  (p=0.000 n=20+20)
    EndToEnd5000-8      8.00 ± 0%      4.00 ± 0%  -50.00%  (p=0.000 n=20+20)

--- internal/context.go
+++ internal/context.go
@@ -3,15 +3,15 @@ package internal
 // ContextKey is a type used for Goji's context.Context keys.
 type ContextKey int
 
-const (
+var (
 	// Path is the context key used to store the path Goji uses for its
 	// PathPrefix optimization.
-	Path ContextKey = iota
+	Path interface{} = ContextKey(0)
 	// Pattern is the context key used to store the Pattern that Goji last
 	// matched.
-	Pattern
+	Pattern interface{} = ContextKey(1)
 	// Handler is the context key used to store the Handler that Goji last
 	// mached (and will therefore dispatch to at the end of the middleware
 	// stack).
-	Handler
+	Handler interface{} = ContextKey(2)
 )
